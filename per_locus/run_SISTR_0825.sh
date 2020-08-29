a3172ae85dadd63c8283f898a45d0a9d88a3ce74
BonnieCSE <bbhuang@ucsd.edu>
2020-08-28 23:23:27 -0700

    add new files

---
+++ per_locus/run_SISTR_0825.sh
@@ -0,0 +1,11 @@
+nohup time python /storage/BonnieH/selection_project/per_locus/SISTR_fast.py 0.005 3 1 4 0.3 /storage/BonnieH/selection_project/ssc_files/0810/allele_freqs_filt.txt y n y 5 eurodem_p1_0825 eurodem_0825 2000 &
+
+nohup time python /storage/BonnieH/selection_project/per_locus/SISTR_fast.py 0.005 3 1 4 0.3 /storage/BonnieH/selection_project/ssc_files/0810/allele_freqs_filt.txt y n y 5 eurodem_p2_0825 eurodem_0825 2000 &
+
+nohup time python /storage/BonnieH/selection_project/per_locus/SISTR_fast.py 0.005 3 1 4 0.3 /storage/BonnieH/selection_project/ssc_files/0810/allele_freqs_filt.txt y n y 5 eurodem_p3_0825 eurodem_0825 2000 &
+
+nohup time python /storage/BonnieH/selection_project/per_locus/SISTR_fast.py 0.005 3 1 4 0.3 /storage/BonnieH/selection_project/ssc_files/0810/allele_freqs_filt.txt y n y 3 eurodem_p1_0825 eurodem_0825 2000 &
+
+nohup time python /storage/BonnieH/selection_project/per_locus/SISTR_fast.py 0.005 3 1 4 0.3 /storage/BonnieH/selection_project/ssc_files/0810/allele_freqs_filt.txt y n y 3 eurodem_p2_0825 eurodem_0825 2000 &
+
+nohup time python /storage/BonnieH/selection_project/per_locus/SISTR_fast.py 0.005 3 1 4 0.3 /storage/BonnieH/selection_project/ssc_files/0810/allele_freqs_filt.txt y n y 3 eurodem_p3_0825 eurodem_0825 2000 &
