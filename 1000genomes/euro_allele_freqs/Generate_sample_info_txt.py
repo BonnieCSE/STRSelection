03ab438cb131550d1609ba747ee7b938815fa381
BonnieCSE <bbhuang@ucsd.edu>
2021-07-25 22:24:47 -0700

    add comments

---
+++ 1000genomes/euro_allele_freqs/Generate_sample_info_txt.py
@@ -0,0 +1,30 @@
+# This script extracts a sample's genotype information from the merged VCF that contains information regarding all samples
+# Example command to run script: python Generate_sample_info_txt.py --sample NA10865
+
+import vcf
+import argparse
+
+# Load arguments from command line
+parser = argparse.ArgumentParser()
+parser.add_argument("--sample")
+    
+args = parser.parse_args()
+
+vcf_reader = vcf.Reader(filename='EUR_sans_FIN_filtered_keep_w_scores.vcf.gz')
+i = 0
+results = open(args.sample + '.txt', "w")
+results.write("chrom" + "\t" + "start" + "\t" + "genotype1" + "\t" + "genotype2" + "\n")
+for record in vcf_reader:
+    i = i + 1
+
+    call = record.genotype(args.sample)
+    if call['REPCN'] is None:
+        geno1 = 0
+        geno2 = 0
+    else:
+        geno1 = call['REPCN'][0]
+        geno2 = call['REPCN'][1]
+
+    results.write(record.CHROM[3:] + '\t' + str(record.POS) + '\t' + str(geno1) + '\t' + str(geno2) + '\n')
+
+results.close()
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 SISTR (Selection Inference at Short Tandem Repeats)  
 
 # Folder Contents  
-* `figures/`: Contains notebooks for generating SISTR2 figures  
-* `helper_functions/`: Contains functions used in notebooks and scripts  
-* `joint_method/`: Contains scripts and notebooks for the joint method  
-* `per_locus/`: Contains script to run SISTR on SSC data
+* `analysis/`: Contains code/results for downstream analysis  
+* `figures/`: Contains notebooks for generating SISTR2 paper figures  
+* `helper_functions/`: Contains helper functions used in notebooks and scripts  
+* `joint_method/`: Contains code to install/run SISTR2 (Joint Method)
+* `per_locus/`: Contains code to run SISTR per-locus method with motif-specific mutation rates
